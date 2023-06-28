b0fe81988fa44c58e4acfdbd707a58f5cbffc6b8
gsovereignty <gsovereignty@proton.me>
2023-06-28 14:22:54 +0800

    problem: not clear how to join the identity tree

--- src/identity/updateProfile.js
+++ src/identity/updateProfile.js
@@ -1,6 +1,6 @@
 import {identities} from "../state/state.js";
 import {makeUnsignedEvent, publish, signAsynchronously} from "../helpers/events.js";
-import {makeH3, makeParagraph, spacer} from "../helpers/markdown.js";
+import {makeH3, makeLink, makeParagraph, spacer} from "../helpers/markdown.js";
 import {makeTextField, makeTextInput} from "../helpers/forms.js";
 import {makeTags} from "../helpers/tags.js";
 import {waitForKind0Ready,getKind0Object,kind0Objects} from "./kind0.js";
@@ -11,12 +11,12 @@ export function updateAccountDetails() {
     form.appendChild(usernameAndBioForm())
     form.appendChild(bioButtons(function () {
         if (document.getElementById( 'name input' ).valueOf().readOnly) {
-            setBio( document.getElementById( 'name input' ).value, document.getElementById( 'about input' ).value )
+            setBio( document.getElementById( 'name input' ).value)
             //location.reload()
         } else {
             validateUnique(document.getElementById( 'name input' ).value).then(res => {
                 if (res) {
-                    setBio( document.getElementById( 'name input' ).value, document.getElementById( 'about input' ).value )
+                    setBio( document.getElementById( 'name input' ).value)
                     //location.reload()
                 } else {
                     alert(document.getElementById( 'name input' ).value + " has been taken, please try another username")
@@ -47,7 +47,7 @@ function bioButtons(onclick) {
     submit.innerText = "Submit"
     let cancel = document.createElement("button")
     cancel.onclick = function () {
-        document.getElementById('name input').value = '';document.getElementById('about input').value = '';
+        document.getElementById('name input').value = '';
     }
     cancel.className = "button is-link is-light"
     cancel.innerText = "Clear"
@@ -65,35 +65,48 @@ function bioButtons(onclick) {
     buttons.appendChild(control)
     return buttons
 }
-function createUsernameAndBioForm(div,haveExistingKind0,username,about){
-
-    div.appendChild(makeH3("Create or modify your Nostrocket profile"))
-    div.appendChild(makeParagraph("* Nostrocket usernames **cannot** be changed once set for your Pubkey   \n* Nostrocket usernames **must** be unique   \n* Protocol: [Non-fungible Identity](superprotocolo://b66541b20c8a05260966393938e2af296c1a39ca5aba8e21bd86fcce2db72715)"))
-    if (haveExistingKind0) {
-        div.appendChild(makeParagraph("Submit this form to claim _**" + kind0Objects.get(window.spaceman.pubkey).name + "**_ now."))
-    }
-    div.appendChild(makeTextInput("Username", "Name or Pseudonym", "name input", 20, username))
-
-    div.appendChild(makeTextField("About Me", "Introduce yourself to the community", "about input", 560, about))
-    return div
-}
-function updateUsernameAndBioForm(div,haveExistingKind0,username,about){
-    div.innerHTML = ""
-    div.appendChild(makeH3("Create or modify your Nostrocket profile"))
-    div.appendChild(makeParagraph("* Nostrocket usernames **cannot** be changed once set for your Pubkey   \n* Nostrocket usernames **must** be unique   \n* Protocol: [Non-fungible Identity](superprotocolo://b66541b20c8a05260966393938e2af296c1a39ca5aba8e21bd86fcce2db72715)"))
-    if (haveExistingKind0) {
-        div.appendChild(makeParagraph("Submit this form to claim _**" + kind0Objects.get(window.spaceman.pubkey).name + "**_ now."))
-    }
-    div.appendChild(makeTextInput("Username", "Name or Pseudonym", "name input", 20, username))
-
-    div.appendChild(makeTextField("About Me", "Introduce yourself to the community", "about input", 560, about))
-    return div
-}
-function usernameAndBioForm() {
+function createUsernameAndBioForm(haveExistingKind0,username){
     let div = document.createElement("div")
+    div.id = window.spaceman.pubkey
+    div.appendChild(paidRelayNotice())
+    div.appendChild(makeH3("Create or modify your Nostrocket Permanym"))
+    div.appendChild(makeParagraph("* The first step to joining the Identity Tree is to set your Permanym   " +
+        "\n* You SHOULD use your existing Nostr pubkey so that existing Nostrocketers can see that you're not a spammer or bad actor   " +
+        "\n* Nostrocket permanyms **cannot** be changed once set for your Pubkey   " +
+        "\n* Nostrocket permanyms **must** be unique   " +
+        "\n* Nostrocket will display your latest Nostr `Kind 0` data, but your Identity Tree permanym will be never change once it is set   " +
+        "\n* Protocol: [Non-fungible Identity](superprotocolo://b66541b20c8a05260966393938e2af296c1a39ca5aba8e21bd86fcce2db72715)"))
+    if (haveExistingKind0) {
+        div.appendChild(makeParagraph("### Found a Kind 0 event for your pubkey: \nSubmit this form to claim *" + kind0Objects.get(window.spaceman.pubkey).name + "* now."))
+    }
+    div.appendChild(makeTextInput("Permanym", "Permanym", "name input", 20, username))
+    return div
+}
+
+function paidRelayNotice() {
+    let div = document.createElement("div")
+    div.className = "notice"
+    div.appendChild(makeParagraph("You MUST be a member of the Nostrocket paid relay to publish events.   " +
+        "\nThis is to prevent spam, and one day it might be enough sats to pay for relay upkeep.   " +
+        "\nThe fee is currently 1000 sats for 2016 blocks."))
+    div.appendChild(makeLink("https://nostr.688.org/join", "Click here to join the relay now"))
+    return div
+}
+
+// function updateUsernameAndBioForm(div,haveExistingKind0,username,about){
+//     div.innerHTML = ""
+//     div.appendChild(makeH3("Create or modify your Nostrocket profile"))
+//     div.appendChild(makeParagraph("* Nostrocket usernames **cannot** be changed once set for your Pubkey   \n* Nostrocket usernames **must** be unique   \n* Protocol: [Non-fungible Identity](superprotocolo://b66541b20c8a05260966393938e2af296c1a39ca5aba8e21bd86fcce2db72715)"))
+//     if (haveExistingKind0) {
+//         div.appendChild(makeParagraph("Submit this form to claim _**" + kind0Objects.get(window.spaceman.pubkey).name + "**_ now."))
+//     }
+//     div.appendChild(makeTextInput("Username", "Name or Pseudonym", "name input", 20, username))
+//
+//     div.appendChild(makeTextField("About Me", "Introduce yourself to the community", "about input", 560, about))
+//     return div
+// }
+function usernameAndBioForm() {
     let username = ""
-    let about = ""
-    let haveExistingIdentity = false
     let haveExistingKind0 = false
     let pubkeyId = identities().filter(item => item.Account === window.spaceman.pubkey)
     if (pubkeyId.length === 0) {
@@ -105,31 +118,30 @@ function usernameAndBioForm() {
                     username = kind0Objects.get(window.spaceman.pubkey).name
                     haveExistingKind0 = true
                 }
-                if (kind0Objects.get(window.spaceman.pubkey).about) {
-                    if (kind0Objects.get(window.spaceman.pubkey).about.length > 0) {
-                        about = kind0Objects.get(window.spaceman.pubkey).about
-                        haveExistingKind0 = true
-                    }
-                }
-                updateUsernameAndBioForm(div,haveExistingKind0,username,about)
+                // if (kind0Objects.get(window.spaceman.pubkey).about) {
+                //     if (kind0Objects.get(window.spaceman.pubkey).about.length > 0) {
+                //         about = kind0Objects.get(window.spaceman.pubkey).about
+                //         haveExistingKind0 = true
+                //     }
+                // }
+
+                document.getElementById(window.spaceman.pubkey).replaceChildren(createUsernameAndBioForm(haveExistingKind0,username))
             }
         })
 
     } else if (pubkeyId.length === 1){
-        haveExistingIdentity = true
-        about = pubkeyId[0].About
         username = pubkeyId[0].Name
     } else {
         alert("Error: multiple identities found for this pubkey.")
     }
-    return createUsernameAndBioForm(div,false,username,about)
+    return createUsernameAndBioForm(haveExistingKind0,username)
 }
 
 
 
-async function setBio(name, about) {
-    if ((name.length > 0) || (about.length > 0)) {
-        let content = JSON.stringify({name: name, about: about})
+async function setBio(name) {
+    if (name.length > 0) {
+        let content = JSON.stringify({name: name})
         let tags = makeTags(window.spaceman.pubkey, "identity")
         let ndkEvent = new NDKEvent(ndk);
         ndkEvent.kind = 640400
