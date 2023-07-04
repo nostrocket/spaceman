510bd006578074327377facd8a96270401285465
gsovereignty <gsovereignty@proton.me>
2023-07-04 22:16:14 +0800

    Problem: loading gif 404

--- src/helpers/loading.js
+++ src/helpers/loading.js
@@ -3,7 +3,7 @@ import {makeParagraph} from "./markdown.js";
 export function loading() {
     let box = document.createElement("div")
     let image = document.createElement("img")
-    image.src = '/loading.gif'
+    image.src = 'loading.gif'
     let waiting = makeParagraph("Waiting for events...")
     box.appendChild(image)
     box.appendChild(waiting)
