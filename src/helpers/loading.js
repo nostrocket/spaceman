fe98229e180ac3b08d07f07aef38e3498917a24c
gsovereignty <gsovereignty@proton.me>
2023-07-04 23:02:39 +0800

    Problem: people's expectations are still too high

--- src/helpers/loading.js
+++ src/helpers/loading.js
@@ -4,7 +4,7 @@ export function loading() {
     let box = document.createElement("div")
     let image = document.createElement("img")
     image.src = 'loading.gif'
-    let waiting = makeParagraph("Waiting for events...")
+    let waiting = makeParagraph("Waiting for events... Be patient, this could take a while.")
     waiting.appendChild(makeParagraph("Spaceman! A Nostrocket client that works sometimes.\n\nIf something goes wrong, hold down the shift key and refresh the page."))
     box.appendChild(image)
     box.appendChild(waiting)
